8fe24a09455d1b088938756ddc1822ba37a4598c
TSVmegaproger228 <148401598+TSVmegaproger228@users.noreply.github.com>
2024-10-24 19:44:55 +0300

    Update task_1.py

--- Лабораторная работа 1/task_1.py
+++ Лабораторная работа 1/task_1.py
@@ -1,8 +1,8 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 sumofnumbs = sum(numbers[:4] + numbers[5:])
 length = len(numbers)
-midd = sumofnumbs / length
-numbers[4] = midd
+middle = sumofnumbs / length
+numbers[4] = middle
 
 # TODO заменить значение пропущенного элемента средним арифметическим
 
