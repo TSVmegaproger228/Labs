6857f67e700eaee0c3beee8fda20de40765a258e
TSVmegaproger228 <148401598+TSVmegaproger228@users.noreply.github.com>
2024-11-04 17:17:50 +0300

    Update task1.py

--- Лабораторная работа 3/task1.py
+++ Лабораторная работа 3/task1.py
@@ -1,14 +1,13 @@
 # TODO Напишите функцию для поиска индекса товара
-def entrance(items_list, find_item):
-    for i in range(len(items_list)):
-        if find_item == items_list[i]:
+def entrance(things_list, thing):
+    for i in range(len(things_list)):
+        if thing == things_list[i]:
             return i
 
 
 items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
-
 for find_item in ['банан', 'груша', 'персик']:
-    index_item = entrance(items_list, find_item) # TODO Вызовите функцию, что получить индекс товара
+    index_item = entrance(items_list, find_item)  # TODO Вызовите функцию, что получить индекс товара
     if index_item is not None:
         print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
     else:
