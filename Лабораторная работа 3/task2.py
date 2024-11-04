105c3bd1b04adc36a594584a2fbf26a70b7d4be9
TSVmegaproger228 <148401598+TSVmegaproger228@users.noreply.github.com>
2024-11-04 17:13:29 +0300

    Update task2.py

--- Лабораторная работа 3/task2.py
+++ Лабораторная работа 3/task2.py
@@ -1,7 +1,7 @@
 # TODO Напишите функцию find_common_participants
 def find_common_participants(first_group, second_group, separator=','):
-    first_group = set(participants_first_group.split(separator))
-    second_group = set(participants_second_group.split(separator))
+    first_group = set(first_group.split(separator))
+    second_group = set(second_group.split(separator))
     general_group = list(first_group.intersection(second_group))
     general_group.sort()
     return general_group
@@ -10,5 +10,5 @@ def find_common_participants(first_group, second_group, separator=','):
 participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
 
-# TODO Провеьте работу функции с разделителем отличным от запятой
+# TODO Проверьте работу функции с разделителем отличным от запятой
 print(find_common_participants(participants_first_group, participants_second_group, '|'))
