3fce13e98af3a044952200c10f2c19a04eac2a4f
TSVmegaproger228 <148401598+TSVmegaproger228@users.noreply.github.com>
2024-11-08 22:44:01 +0300

    Update task2.py

--- Лабораторная работа 3/task2.py
+++ Лабораторная работа 3/task2.py
@@ -2,9 +2,9 @@
 def find_common_participants(first_list, second_list, separator=','):
     first_group = set(first_list.split(separator))
     second_group = set(second_list.split(separator))
-    general_group = list(first_group.intersection(second_group))
-    general_group.sort()
-    return general_group
+    common_group = list(first_group.intersection(second_group))
+    common_group.sort()
+    return common_group
 
 
 participants_first_group = "Иванов|Петров|Сидоров"
